95cfa4908e241df1b1cb33334e040f4a0897e5c3
Marco <marco.gorgetti.2@gmail.com>
2025-09-18 17:32:01 +0200

    send email setup

---
+++ netlify/functions/mail/index.mts
@@ -0,0 +1,41 @@
+import type { Handler } from '@netlify/functions';
+import fetch from 'node-fetch';
+
+const email: string = 'website@virgosites.com';
+
+const handler: Handler = async function (event) {
+	if (event.body === null) {
+		return {
+			statusCode: 400,
+			body: JSON.stringify('Payload required')
+		};
+	}
+
+	//page content
+	const requestBody = JSON.parse(event.body) as {
+		name: string;
+		data: string;
+	};
+
+    
+	//automatically generated snippet from the email preview
+	//sends a request to an email handler for a subscribed email
+	await fetch(`${process.env.URL}/.netlify/functions/emails/message`, {
+		headers: {
+			'netlify-emails-secret': process.env.NETLIFY_EMAILS_SECRET as string
+		},
+		method: 'POST',
+		body: JSON.stringify({
+			from: email,
+			to: email,
+			subject: `${requestBody.name} - ${requestBody.data}`
+		})
+	});
+
+	return {
+		statusCode: 200,//OK
+		body: JSON.stringify('Email sent!')
+	};
+};
+
+export { handler };
--- src/lib/components/Contact.svelte
+++ src/lib/components/Contact.svelte
@@ -1,23 +1,24 @@
 <script lang="ts">
-	import { Label, Input, Helper } from 'flowbite-svelte';
+	import { Label, Input } from 'flowbite-svelte';
+
+	let name = $state('');
+
+	const sendReq = () => {
+		const data = { name: name, data: '' };
+		fetch('/.netlify/functions/mail', {
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json'
+			},
+			method: 'POST',
+			body: JSON.stringify(data)
+		});
+	};
 </script>
 
-<form action="?/mail" method="post">
-	<!--RADIO-->
-
-	<!--MAIL-->
-	<Label class="mb-2 block">Your email</Label>
-	<Input id="email" name="email" required placeholder="name@flowbite.com" />
-	<!--TEL-->
-	<Label class="mb-2 block">Your email</Label>
-	<Input id="email" name="email" required placeholder="name@flowbite.com" />
-
-	<Helper class="mt-2 text-sm">
-		We’ll never share your details. Read our <a
-			href="/"
-			class="font-medium text-primary-600 hover:underline dark:text-primary-500">Privacy Policy</a
-		>
-		.
-	</Helper>
-	<input type="submit" value="invia" />
-</form>
+<div>
+	<!--NAME-->
+	<Label class="mb-2 block">Nome</Label>
+	<Input id="email" name="email" bind:value={name} required placeholder="Antonio" />
+	<button onclick={() => sendReq()}> Invia </button>
+</div>
