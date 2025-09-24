307016af11e7d1342529bb55c8c87e674572ce3c
Marco <marco.gorgetti.2@gmail.com>
2025-09-24 13:11:57 +0200

    ui fixes

--- src/lib/components/static/Metodo.svelte
+++ src/lib/components/static/Metodo.svelte
@@ -8,37 +8,39 @@
 	});
 </script>
 
-<div class="h-fit w-full snap-center p-4 lg:p-12" id="people-beyond-ai">
+<div class="h-fit w-full snap-center lg:p-12" id="people-beyond-ai">
 	<div
-		class="h-full w-full flex-col gap-0 overflow-hidden rounded-[12px] border border-gray-200/50 bg-gray-200/8 p-4 lg:p-24"
+		class="h-full w-full flex-col gap-0 overflow-hidden border-gray-200/50 bg-gray-200/8 p-4 pt-10 lg:rounded-[12px] lg:border lg:p-24"
 	>
-		<h1 class="mb-4 p-8 text-4xl font-medium lg:p-0">Il nostro metodo</h1>
-		<p class="pb-4 text-xl! lg:max-w-1/3">
-			Una visione che mette al centro i tuoi collaboratori e il tuo team, miglioriamo le performance
-			con strumenti avanzati ma che aiutano concretamente il tuo lavoro.
-		</p>
-		<div class="flex flex-col gap-4 sm:mt-16 lg:grid-cols-3 lg:grid-rows-2 lg:flex-row">
+		<div class="p-4 lg:pb-10">
+			<h1 class="mb-4 text-4xl font-medium">Il nostro metodo</h1>
+			<p class="pb-10 text-xl! lg:max-w-1/3 lg:pb-4">
+				Una visione che mette al centro i tuoi collaboratori e il tuo team, miglioriamo le
+				performance con strumenti avanzati ma che aiutano concretamente il tuo lavoro.
+			</p>
+		</div>
+		<div class="flex flex-col gap-4 lg:grid-cols-3 lg:grid-rows-2 lg:flex-row">
 			<!--lg:mt-20 lg:-mb-20-->
 			<div class="relative lg:row-span-2 lg:min-w-1/3">
 				<div class="relative flex h-full flex-col overflow-hidden rounded-[12px]">
-					<div class="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
+					<div class="px-8 pt-8 pb-3">
 						<h1 class="text-4xl font-medium">1.</h1>
 						<div class="py-4">
 							<Accordion flush>
 								<AccordionItem>
-									{#snippet header()}<p class="mt-2 text-xl! font-medium max-lg:text-center">
+									{#snippet header()}<p class="mt-2 text-xl! font-medium">
 											Strategia e analisi del contesto
 										</p>{/snippet}
-									<p class="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+									<p class="mt-2 max-w-lg text-sm/6 text-gray-600">
 										Ogni progetto parte da una fase di valutazione e pianificazione, in cui
 										analizziamo la maturità digitale della tua azienda e gli obiettivi che desideri
 										raggiungere.
 									</p>
-									<p class="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+									<p class="mt-2 max-w-lg text-sm/6 text-gray-600">
 										Attraverso workshop e confronti diretti con il tuo team, costruiamo una roadmap
 										strategica che bilancia innovazione e concretezza.
 									</p>
-									<p class="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+									<p class="mt-2 max-w-lg text-sm/6 text-gray-600">
 										Questo approccio ci permette di identificare le opportunità a maggior impatto,
 										evitando soluzioni improvvisate o non collaudate. L’obiettivo è creare basi
 										solide affinché la tecnologia diventi un alleato reale, in linea con i tuoi
@@ -48,16 +50,18 @@
 							</Accordion>
 						</div>
 					</div>
-					<div class="@container relative min-h-120 w-full grow max-lg:mx-auto max-lg:max-w-sm">
+					<div class="relative h-[20vh] min-h-60 w-full grow px-10 lg:h-full">
 						<div
-							class="absolute inset-x-10 top-10 bottom-0 grid place-items-center overflow-hidden rounded-t-[12px] bg-gray-200/18"
+							class="top-10 bottom-0 grid h-full place-items-center overflow-hidden rounded-t-[12px] bg-gray-200/18 lg:relative"
 						>
-							<div
-								class="rotate-loop relative -top-[40%] -left-[50%] h-240 w-240 rounded-[39%] border-2 border-brand-500 bg-brand-500/30"
-							></div>
-							<div
-								class="rotate-loop relative -top-[80%] left-[15%] h-240 w-240 rounded-[41%] border-2 border-brand-500 bg-brand-500/30"
-							></div>
+							<div class="relative flex w-full flex-col items-center overflow-hidden lg:absolute">
+								<div
+									class="rotate-loop -my-10 h-100 w-100 rounded-[40%] border-2 border-brand-500 bg-radial from-transparent from-[70%] to-brand-500/80 lg:h-240 lg:w-240"
+								></div>
+								<div
+									class="rotate-loop -my-10 h-100 w-100 rounded-[40%] border-2 border-brand-500 bg-radial from-transparent from-[70%] to-brand-500/80 lg:h-240 lg:w-240"
+								></div>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -68,24 +72,24 @@
 			<div class="flex w-full flex-col gap-4">
 				<div class="relative max-lg:row-start-1">
 					<div class="relative flex h-fit flex-col overflow-hidden rounded-[12px]">
-						<div class="px-8 pt-8 sm:px-10 sm:pt-10">
+						<div class="px-8 pt-8">
 							<h1 class="text-4xl font-medium">2.</h1>
 							<div class="py-4">
 								<Accordion flush>
 									<AccordionItem>
-										{#snippet header()}<p class=" text-xl! font-medium max-lg:text-center">
+										{#snippet header()}<p class=" text-xl! font-medium">
 												Progettazione collaborativa e soluzioni su misura
 											</p>{/snippet}
-										<p class="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+										<p class="mt-2 max-w-lg text-sm/6 text-gray-600">
 											Non crediamo in modelli standardizzati, ma in soluzioni che nascono dalla
 											conoscenza interna della tua azienda e dalle competenze delle persone che vi
 											lavorano.
 										</p>
-										<p class="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+										<p class="mt-2 max-w-lg text-sm/6 text-gray-600">
 											Per questo, ci immergiamo nel tuo settore, nei processi e nelle sfide
 											quotidiane, così da capire come l’AI possa generare valore concreto.
 										</p>
-										<p class="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+										<p class="mt-2 max-w-lg text-sm/6 text-gray-600">
 											Disegniamo architetture scalabili e integrate con gli strumenti che già
 											utilizzi, scegliendo tecnologie affidabili e testate sul campo. In questo
 											modo, l’AI diventa un supporto intelligente che amplifica l’esperienza delle
@@ -96,18 +100,20 @@
 								</Accordion>
 							</div>
 						</div>
-						<div class="@container relative min-h-120 w-full grow max-lg:mx-auto max-lg:max-w-sm">
-							<div
-								class="absolute inset-x-10 top-10 bottom-0 grid place-items-center overflow-hidden rounded-t-[12px] bg-gray-200/18"
-							>
+						<div class="relative h-[20vh] min-h-60 w-full grow px-10 lg:h-full">
+						<div
+							class="top-10 bottom-0 grid h-full place-items-center overflow-hidden rounded-t-[12px] bg-gray-200/18 lg:relative"
+						>
+							<div class="relative flex w-full flex-col items-center overflow-hidden lg:absolute">
 								<div
-									class="rotate-loop relative -top-[40%] -left-[50%] h-240 w-240 rounded-[39%] border-2 border-brand-500 bg-brand-500/30"
+									class="rotate-loop -my-10 h-100 w-100 rounded-[40%] border-2 border-brand-500 bg-radial from-transparent from-[70%] to-brand-500/80 lg:h-240 lg:w-240"
 								></div>
 								<div
-									class="rotate-loop relative -top-[80%] left-[15%] h-240 w-240 rounded-[41%] border-2 border-brand-500 bg-brand-500/30"
+									class="rotate-loop -my-10 h-100 w-100 rounded-[40%] border-2 border-brand-500 bg-radial from-transparent from-[70%] to-brand-500/80 lg:h-240 lg:w-240"
 								></div>
 							</div>
 						</div>
+					</div>
 					</div>
 					<div
 						class="pointer-events-none absolute inset-px rounded-[12px] shadow-sm outline outline-black/5"
@@ -116,25 +122,25 @@
 				<div class="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2">
 					<div class="absolute inset-px rounded-lg bg-white"></div>
 					<div class="relative flex h-full flex-col overflow-hidden rounded-[12px]">
-						<div class="px-8 pt-8 sm:px-10 sm:pt-10">
+						<div class="px-8 pt-8">
 							<h1 class="text-4xl font-medium">3.</h1>
 							<div class="py-4">
 								<Accordion flush>
 									<AccordionItem>
-										{#snippet header()}<p class="mt-2 text-xl! font-medium max-lg:text-center">
+										{#snippet header()}<p class="mt-2 text-xl! font-medium">
 												Implementazione, adozione, miglioramento continuo
 											</p>{/snippet}
-										<p class="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+										<p class="mt-2 max-w-lg text-sm/6 text-gray-600">
 											Trasformiamo le idee in sistemi funzionanti, progettati per adattarsi al
 											contesto reale della tua azienda e non solo a scenari teorici.
 										</p>
-										<p class="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+										<p class="mt-2 max-w-lg text-sm/6 text-gray-600">
 											Seguiamo passo dopo passo l’adozione delle nuove soluzioni, affiancando il tuo
 											team con formazione mirata e strumenti di change management. Questo garantisce
 											che la tecnologia venga accolta e compresa, anziché percepita come un
 											ostacolo.
 										</p>
-										<p class="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+										<p class="mt-2 max-w-lg text-sm/6 text-gray-600">
 											Una volta implementato il sistema, monitoriamo i risultati attraverso KPI e
 											cicli di miglioramento continuo, così da ottimizzare le performance e generare
 											valore crescente nel tempo. L’AI diventa così un percorso evolutivo, in cui le
@@ -144,16 +150,18 @@
 								</Accordion>
 							</div>
 						</div>
-						<div class="@container relative min-h-120 w-full grow max-lg:mx-auto max-lg:max-w-sm">
+						<div class="relative h-[20vh] min-h-60 w-full grow px-10 lg:h-full">
 							<div
-								class="absolute inset-x-10 top-10 bottom-0 grid place-items-center overflow-hidden rounded-t-[12px] bg-gray-200/18"
+								class="top-10 bottom-0 grid h-full place-items-center overflow-hidden rounded-t-[12px] bg-gray-200/18 lg:relative"
 							>
-								<div
-									class="rotate-loop relative -top-[40%] -left-[50%] h-240 w-240 rounded-[39%] border-2 border-brand-500 bg-brand-500/30"
-								></div>
-								<div
-									class="rotate-loop relative -top-[80%] left-[15%] h-240 w-240 rounded-[41%] border-2 border-brand-500 bg-brand-500/30"
-								></div>
+								<div class="relative flex w-full flex-col items-center overflow-hidden lg:absolute">
+									<div
+										class="rotate-loop -my-10 h-100 w-100 rounded-[40%] border-2 border-brand-500 bg-radial from-transparent from-[70%] to-brand-500/80 lg:h-240 lg:w-240"
+									></div>
+									<div
+										class="rotate-loop -my-10 h-100 w-100 rounded-[40%] border-2 border-brand-500 bg-radial from-transparent from-[70%] to-brand-500/80 lg:h-240 lg:w-240"
+									></div>
+								</div>
 							</div>
 						</div>
 					</div>
@@ -166,23 +174,23 @@
 			<div class="relative lg:row-span-2 lg:min-w-1/3">
 				<div class="absolute inset-px rounded-[12px] bg-white"></div>
 				<div class="relative flex h-full flex-col overflow-hidden rounded-[12px]">
-					<div class="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
+					<div class="px-8 pt-8 pb-3">
 						<h1 class="text-4xl font-medium">4.</h1>
 						<div class="py-4">
 							<Accordion flush>
 								<AccordionItem>
-									{#snippet header()}<p class="mt-2 text-xl! font-medium max-lg:text-center">
+									{#snippet header()}<p class="mt-2 text-xl! font-medium">
 											Support non stop, 24/7!
 										</p>{/snippet}
-									<p class="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+									<p class="mt-2 max-w-lg text-sm/6 text-gray-600">
 										Mettici alla prova e scoprirai che il nostro team di assistenza - fatto di
 										persone - sarà sempre al tuo fianco.
 									</p>
-									<p class="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+									<p class="mt-2 max-w-lg text-sm/6 text-gray-600">
 										Ti garantiamo un supporto costante, monitorando le soluzioni implementate e
 										intervenendo tempestivamente ogni volta che serve.
 									</p>
-									<p class="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+									<p class="mt-2 max-w-lg text-sm/6 text-gray-600">
 										La nostra presenza non si limita alla fase iniziale: restiamo al tuo fianco nel
 										tempo, accompagnandoti in ogni evoluzione. E se ti serve una mano fuori dagli
 										orari tradizionali, puoi contare su di noi anche durante il weekend.
@@ -191,17 +199,19 @@
 							</Accordion>
 						</div>
 					</div>
-						<div class="@container relative min-h-120 w-full h-full grow max-lg:mx-auto max-lg:max-w-sm">
+					<div class="relative h-[20vh] min-h-60 w-full grow px-10 lg:h-full">
 						<div
-							class="absolute inset-x-10 top-10 bottom-0 grid place-items-center overflow-hidden rounded-t-[12px] bg-gray-200/18"
+							class="top-10 bottom-0 grid h-full place-items-center overflow-hidden rounded-t-[12px] bg-gray-200/18 lg:relative"
 						>
-							<div
-								class="rotate-loop relative -top-[40%] -left-[50%] h-240 w-240 rounded-[39%] border-2 border-brand-500 bg-brand-500/30"
-							></div>
-							<div
-								class="rotate-loop relative -top-[80%] left-[15%] h-240 w-240 rounded-[41%] border-2 border-brand-500 bg-brand-500/30"
-							></div>
-					</div>
+							<div class="relative flex w-full flex-col items-center overflow-hidden lg:absolute">
+								<div
+									class="rotate-loop -my-10 h-100 w-100 rounded-[40%] border-2 border-brand-500 bg-radial from-transparent from-[70%] to-brand-500/80 lg:h-240 lg:w-240"
+								></div>
+								<div
+									class="rotate-loop -my-10 h-100 w-100 rounded-[40%] border-2 border-brand-500 bg-radial from-transparent from-[70%] to-brand-500/80 lg:h-240 lg:w-240"
+								></div>
+							</div>
+						</div>
 					</div>
 				</div>
 				<div
--- src/lib/components/static/Payoff.svelte
+++ src/lib/components/static/Payoff.svelte
@@ -6,19 +6,19 @@
 		<div class="relative">
 			<div class="onscroll-enter relative z-100 flex h-full w-full flex-col items-center">
 				<h1 class="mb-4 p-8 text-center text-4xl font-medium lg:p-0">People Beyond AI</h1>
-				<p class="pb-4 text-center text-xl! lg:max-w-2/3">
+				<p class="pb-4 text-center text-xl! lg:max-w-2/5">
 					Una visione che mette al centro i tuoi collaboratori e il tuo team, miglioriamo le
 					performance con strumenti avanzati ma che aiutano concretamente il tuo lavoro.
 				</p>
 			</div>
 			<span class="absolute -top-[25%] left-[50%] z-10 flex size-120 -translate-x-[50%] lg:-top-40">
 				<span
-					class="animate-ping-v2 absolute inline-flex h-full w-full rounded-full border-1 border-brand-500"
+					class="animate-ping-v2 absolute inline-flex h-full w-full rounded-full border-1 border-brand-500 bg-radial from-transparent from-[65%] to-brand-500/80"
 				></span>
 			</span>
 			<span class="absolute top-0 left-[50%] z-10 flex size-80 -translate-x-[50%] lg:-top-20">
 				<span
-					class="animate-ping-v1 absolute inline-flex h-full w-full rounded-full border-1 border-brand-500"
+					class="animate-ping-v1 absolute inline-flex h-full w-full rounded-full border-1 border-brand-500 bg-radial from-transparent from-[65%] to-brand-500/80"
 				></span>
 			</span>
 		</div>
@@ -27,14 +27,16 @@
 
 <style>
 	.animate-ping-v1 {
-		animation: ping 3s cubic-bezier(0, 0, 0.2, 1) infinite;
+		animation: ping 5s cubic-bezier(0, 0, 0.2, 1) infinite;
 	}
 	.animate-ping-v2 {
-		animation: ping 3s cubic-bezier(0, 0, 0.8, 1) infinite;
+		animation: ping 5s cubic-bezier(0, 0, 0.8, 1) infinite;
 	}
 
 	@keyframes ping {
-		75%,
+		55% {
+			opacity: 1;
+		}
 		100% {
 			transform: scale(2);
 			opacity: 0;
