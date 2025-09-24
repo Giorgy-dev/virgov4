dd2dd5d15e85ff55ce4023eb453d08cc00dd3817
Marco <marco.gorgetti.2@gmail.com>
2025-09-24 18:55:37 +0200

    fixd payoff section issue

--- src/lib/components/static/Payoff.svelte
+++ src/lib/components/static/Payoff.svelte
@@ -1,17 +1,21 @@
 <!-- PAYOFF -->
-<div class="h-svh w-full snap-center p-1 lg:p-4 lg:p-12" id="people-beyond-ai">
+<div class="h-svh w-full snap-center p-1 lg:p-12" id="people-beyond-ai">
 	<div
-		class="grid h-full w-full flex-col place-items-center gap-0 overflow-hidden rounded-[24px] border border-gray-200/50 bg-gray-200/8 p-1 lg:p-4 lg:p-12"
+		class="grid h-full w-full flex-col place-items-center gap-0 overflow-hidden rounded-[24px] border border-gray-200/50 bg-gray-200/8 p-1 lg:p-12"
 	>
 		<div class="relative">
 			<div class="relative z-100 flex h-full w-full flex-col items-center">
-				<h1 class="mb-4 p-8 text-center text-4xl font-medium lg:p-0">People Beyond AI</h1>
+				<h1 class="mb-4 p-8 px-12 text-center text-4xl font-medium lg:p-0 lg:px-0">
+					People Beyond AI
+				</h1>
 				<p class=" pb-4 text-center text-xl! lg:max-w-2/5">
 					Una visione che mette al centro i tuoi collaboratori e il tuo team, miglioriamo le
 					performance con strumenti avanzati ma che aiutano concretamente il tuo lavoro.
 				</p>
 			</div>
-			<span class="absolute -top-[25%] left-[50%] z-10 flex size-120 -translate-x-[50%] lg:-top-1 lg:p-40">
+			<span
+				class="absolute -top-[25%] left-[50%] z-10 flex size-120 -translate-x-[50%] lg:-translate-x-[50%] lg:-translate-y-[25%]"
+			>
 				<span
 					class="animate-ping-v2 absolute inline-flex h-full w-full rounded-full border-1 border-brand-500 bg-radial from-transparent from-[65%] to-brand-500/80"
 				></span>
